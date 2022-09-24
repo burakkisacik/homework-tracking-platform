49e79efeb3594d9bd9c1897375b8e1ae94ab2ae0
Burak KISACIK <burakkisacik@yahoo.com>
2022-09-24 17:11:57 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDz7Iz2NLIEshslZWeS7nUt8UTzhxIqqJY",
-  authDomain: "homeworktrackingapp-753bf.firebaseapp.com",
-  projectId: "homeworktrackingapp-753bf",
-  storageBucket: "homeworktrackingapp-753bf.appspot.com",
-  messagingSenderId: "403569008101",
-  appId: "1:403569008101:web:017ca39a8296622e8ba30c",
+  apiKey: "***",
+  authDomain: "***",
+  projectId: "***",
+  storageBucket: "***",
+  messagingSenderId: "***",
+  appId: "***",
 };
 
 // Initialize Firebase
